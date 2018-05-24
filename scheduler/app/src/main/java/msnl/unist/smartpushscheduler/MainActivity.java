1fc74aa16204de402758ed5d5aff5a6897f22a04
lovgrammer <30390634+lovgrammer@users.noreply.github.com>
2018-05-24 15:51:48 +0900

    minor change

--- scheduler/app/src/main/java/msnl/unist/smartpushscheduler/MainActivity.java
+++ scheduler/app/src/main/java/msnl/unist/smartpushscheduler/MainActivity.java
@@ -2,6 +2,7 @@ package msnl.unist.smartpushscheduler;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View.OnClickListener;
@@ -11,28 +12,34 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     
-    private Button mButton;
+    private Button mNotiButton;
+    private Button mAccButton;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-	mButton = (Button) findViewById(R.id.btn_start);
-	mButton.setOnClickListener(onButtonsClick);
+	mNotiButton = (Button) findViewById(R.id.btn_noti_permission);
+	mAccButton = (Button) findViewById(R.id.btn_acc_permission);
+	mNotiButton.setOnClickListener(onButtonsClick);
+	mAccButton.setOnClickListener(onButtonsClick);
     }
 
     OnClickListener onButtonsClick = new OnClickListener() {
 	    @Override
 	    public void onClick(View v) {
 		switch(v.getId()) {
-		case R.id.btn_start:
-		    boolean  isPermissionAllowed = isNotiPermissionAllowed();
+		case R.id.btn_noti_permission:
+		    boolean isPermissionAllowed = isNotiPermissionAllowed();
 		    if (!isPermissionAllowed) {
-		    	Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
-		    	startActivity(intent);
+			Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+			startActivity(intent);
 		    }
-		    Intent intent = new Intent(MainActivity.this, SPScheduleService.class);
-		    startService(intent);
+		    // Intent intent = new Intent(MainActivity.this, SPScheduleService.class);
+		    // startService(intent);
+		    break;
+		case R.id.btn_acc_permission:
+		    startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
 		    break;
 		}
 	    }
